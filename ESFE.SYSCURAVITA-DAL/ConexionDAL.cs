0fb56678a23b7eae5c9b93f4fa3e8209262649f1
ALEX SALINAS <jmalexander2007@gmail.com>
2026-08-28 22:01:26 -0600

    actu

--- ESFE.SYSCURAVITA-DAL/ConexionDAL.cs
+++ ESFE.SYSCURAVITA-DAL/ConexionDAL.cs
@@ -7,5 +7,6 @@ namespace ESFE.SYSCURAVITA_DAL
         private const string Cadena = "Data Source=sql5106.site4now.net;Initial Catalog=db_acdbf3_clinicadb;User Id=db_acdbf3_clinicadb_admin;Password=K9#mP2$xL8!v;Encrypt=True;TrustServerCertificate=True;";
 
         public static SqlConnection ObtenerConexion() => new(Cadena);
+        //
     }
 }
